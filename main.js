cd8fed54e0e86c0799064172e2affc085dec9f7c
Dovi38 <64047061+Dovi38@users.noreply.github.com>
2024-04-15 20:55:35 +0100

    change e to Event

--- main.js
+++ main.js
@@ -38,6 +38,7 @@ const resetGame = () => {
   removeActivePlayer();
   removeActiveComputer();
   messageDisplay.innerText = "Let's play again";
+  messageDisplay.style.color = "black";
   computer.innerText = "Computer score: 0";
   player.innerText = "Player score: 0";
   playerScore = 0;
@@ -52,12 +53,12 @@ const resetGame = () => {
 };
 resetBtn.addEventListener("click", resetGame);
 //player's clicked icon
-const getPlayerChoice = (e, i) => {
+const getPlayerChoice = (Event, i) => {
   if (userChoice) {
     removeActivePlayer();
     playerIcons[i].classList.add("active");
 
-    let clickedIcon = e.target.classList.value;
+    let clickedIcon = Event.target.classList.value;
     if (clickedIcon.includes("fa-hand-back-fist")) {
       playerSelection = "rock";
     } else if (clickedIcon.includes("paper")) {
@@ -110,27 +111,27 @@ const compareScores = (playerSelection, computerSelection) => {
     result = "The Result is equal. Tie";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore++;
-    computer.innerText = `Computer score ${computerScore}`;
+    computer.innerText = `Computer score: ${computerScore}`;
     result = "The Paper covers the Rock. Computer WON";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore++;
-    player.innerText = `Player score ${playerScore}`;
+    player.innerText = `Player score: ${playerScore}`;
     result = "The Scissors can't cut the Rock. Player WON";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore++;
-    player.innerText = `Player score ${playerScore}`;
+    player.innerText = `Player score: ${playerScore}`;
     result = "The Paper covers the Rock. Player WON";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerScore++;
-    computer.innerText = `Computer score ${computerScore}`;
+    computer.innerText = `Computer score: ${computerScore}`;
     result = "The Scissors cuts the Paper. Computer WON";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     computerScore++;
-    computer.innerText = `Computer score ${computerScore}`;
+    computer.innerText = `Computer score: ${computerScore}`;
     result = "The Scissors can't cut the Paper. Computer WON";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore++;
-    player.innerText = `Player score ${playerScore}`;
+    player.innerText = `Player score: ${playerScore}`;
     result = "The Scissors cuts the Paper. Player WON";
   } else {
     return;
@@ -141,16 +142,20 @@ const compareScores = (playerSelection, computerSelection) => {
 };
 
 for (let i = 0; i < playerIcons.length; i++) {
-  playerIcons[i].addEventListener("click", (e) => getPlayerChoice(e, i));
+  playerIcons[i].addEventListener("click", (Event) =>
+    getPlayerChoice(Event, i)
+  );
 }
 const gameEnd = () => {
   if (playerScore === 5) {
     userChoice = false;
     stopClick();
-    return (messageDisplay.innerText = `Player Beats Computer`);
+    messageDisplay.style.color = "red";
+    return (messageDisplay.innerText = `Player Beats Computer!`);
   } else if (computerScore === 5) {
     userChoice = false;
     stopClick();
-    return (messageDisplay.innerText = `Computer Won. Let's try again`);
+    messageDisplay.style.color = "red";
+    return (messageDisplay.innerText = `Computer Beats Player!`);
   }
 };
